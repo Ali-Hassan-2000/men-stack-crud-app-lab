1b0f2f33f3fce43297fa67b38db26f621cf50597
Dell <ali.hassan.salman.2000@gmail.com>
2025-09-10 17:29:15 +0300

    Task 5

--- model/Car.js
+++ model/Car.js
@@ -6,7 +6,7 @@ const carSchema = new mongoose.Schema({
   name: { type: String, required: true },
   carModel: { type: Number, required: true },
   carColor: { type: String, required: true },
-  carNumber: { type: String, required: true, unique: true },
+  carNumber: { type: Number, required: true, unique: true },
   Millage: { type: Number, required: true },
 });
 
--- server.js
+++ server.js
@@ -52,9 +52,66 @@ app.get("/", async (req, res) => {
 
 // 5. Create routes and views for CRUD operations
 
-app.get("/new", async (req, res) => {
-  res.render("");
+// GET /
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
 });
+
+// GET /cars/new
+app.get("/cars/new", (req, res) => {
+  res.render("cars/new.ejs");
+});
+
+// GET /cars/:id (this must be after new path)
+app.get("/cars/:carId", async (req, res) => {
+  const foundCar = await Car.findById(req.params.carId);
+  res.render("cars/show.ejs", { Car: foundCar });
+});
+
+// POST /cars
+app.post("/cars", async (req, res) => {
+  
+  await Car.create(req.body);
+  res.redirect("/cars/new");
+});
+
+// GET /cars
+app.get("/cars", async (req, res) => {
+  const allcars = await Car.find();
+  //console.log(allcars); // log the cars!
+  res.render("cars/index.ejs", { cars: allcars });
+});
+
+// delete function
+app.delete("/cars/:carId", async (req, res) => {
+  await Car.findByIdAndDelete(req.params.carId);
+  res.redirect("/cars");
+});
+
+// delete Item route
+app.delete("/cars/:carId", (req, res) => {
+  res.send("This is the delete route");
+});
+
+// GET localhost:3000/cars/:carId/edit
+app.get("/cars/:carId/edit", async (req, res) => {
+  const foundCar = await Car.findById(req.params.carId);
+  //console.log(foundCar);
+  res.render("cars/edit.ejs", {
+    car: foundCar,
+  });
+});
+
+// update function
+app.put("/cars/:carId", async (req, res) => {
+  
+  // Update the car in the database
+  await Car.findByIdAndUpdate(req.params.carId, req.body);
+
+  // Redirect to the car's show page to see the updates
+  res.redirect(`/cars/${req.params.carId}`);
+});
+
 /* ----------------------------- TCP ---------------------------------- */
 app.listen(3000, () => {
   console.log('Listening on port 3000');
